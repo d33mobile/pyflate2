a5a47ead71b6a0a3adb717074fbcc454c7870058
Jacek Wielemborek <d33tah@gmail.com>
2024-09-15 17:22:54 +0200

    add unit tests for bitfields, 100% branch cov

--- pyflate/bit.py
+++ pyflate/bit.py
@@ -14,6 +14,10 @@ def log(*args: T.Any) -> None:
     logging.debug(" ".join(map(str, args)))
 
 
+class LengthError(Exception):
+    pass
+
+
 class BitfieldBase:
     def __init__(self, x: T.Union[T.BinaryIO, "BitfieldBase"]) -> None:
         if isinstance(x, BitfieldBase):
@@ -30,38 +34,41 @@ class BitfieldBase:
 
     def _read(self, n: int) -> bytes:
         s = self.f.read(n)
+        log("BitfieldBase._read(%d) -> %r" % (n, s))
         if not s:
-            raise Exception("Length Error")
+            raise LengthError()
         self.count += len(s)
         return s
 
     def needbits(self, n: int) -> None:
+        log("BitfieldBase.needbits(%d)" % n)
         while self.bits < n:
             self._more()
 
-    def _mask(self, n: int) -> int:
+    @staticmethod
+    def _mask(n: int) -> int:
         return (1 << n) - 1
 
     def toskip(self) -> int:
-        return self.bits & 0x7
+        return self.bits & 0b111
 
     def align(self) -> None:
         self.readbits(self.toskip())
 
     def tell(self) -> T.Tuple[int, int]:
-        return self.count - ((self.bits + 7) >> 3), 7 - ((self.bits - 1) & 0x7)
+        return self.count - ((self.bits + 7) >> 3), 7 - ((self.bits - 1) & 0b111)
 
     def tellbits(self) -> int:
         bytes, bits = self.tell()
         return (bytes << 3) + bits
 
-    def readbits(self, n: int = 8) -> int:
+    def readbits(self, n: int = 8) -> int:  # pragma: no cover
         raise NotImplementedError()
 
-    def snoopbits(self, n: int = 8) -> int:
+    def snoopbits(self, n: int = 8) -> int:   # pragma: no cover
         raise NotImplementedError()
 
-    def _more(self) -> None:
+    def _more(self) -> None:  # pragma: no cover
         raise NotImplementedError()
 
 
@@ -94,6 +101,7 @@ class RBitfield(BitfieldBase):
 
     def snoopbits(self, n: int = 8) -> int:
         if n > self.bits:
+            log("RBitfield.snoopbits(%d) needbits(%d)" % (n, n))
             self.needbits(n)
         return (self.bitfield >> (self.bits - n)) & self._mask(n)
 
@@ -104,3 +112,68 @@ class RBitfield(BitfieldBase):
         self.bits -= n
         self.bitfield &= ~(self._mask(n) << self.bits)
         return r
+
+
+import unittest
+import io
+
+
+class TestBitfield(unittest.TestCase):
+    def test_bitfield(self) -> None:
+        b = Bitfield(io.BytesIO(b"\x01"))
+        self.assertEqual(b.readbits(1), 1)
+        self.assertEqual(b.tell(), (0, 1))
+        self.assertEqual(b.readbits(1), 0)
+        self.assertEqual(b.tell(), (0, 2))
+        try:
+            b.readbits(8)
+            self.fail("expected exception")  # pragma: no cover
+        except LengthError:
+            pass
+    def test_snoop(self) -> None:
+        digit = 0b0110
+        b = Bitfield(io.BytesIO(bytes([digit, digit])))
+        self.assertEqual(b.snoopbits(2), 0b10)
+        # once again, no needbits now
+        self.assertEqual(b.snoopbits(2), 0b10)
+        self.assertEqual(b.readbits(2), 0b10)
+        # trigger the needbit
+        # todo: readability
+        self.assertEqual(b.snoopbits(8), 129)
+        self.assertEqual(b.readbits(8), 129)
+    def test_snoop_r(self) -> None:
+        digit = 0b10110110
+        b = RBitfield(io.BytesIO(bytes([digit] * 4)))
+        self.assertEqual(b.snoopbits(2), 0b10)
+        self.assertEqual(b.snoopbits(2), 0b10)
+        self.assertEqual(b.readbits(2), 0b10)
+        # trigger the needbit for snoop
+        # todo: readability
+        self.assertEqual(b.snoopbits(8), 218)
+        self.assertEqual(b.readbits(8), 218)
+        # trigger the needbit for read
+        # todo: readability
+        self.assertEqual(b.readbits(8), 218)
+        self.assertEqual(b.snoopbits(8), 218)
+    def test_construct(self) -> None:
+        b = Bitfield(io.BytesIO(b"\x01"))
+        b2 = Bitfield(b)
+        self.assertEqual(b2.readbits(1), 1)
+    def test_to_skip(self) -> None:
+        b = Bitfield(io.BytesIO(bytes([0b10101])))
+        self.assertEqual(b.bits, 0)
+        self.assertEqual(b.toskip(), 0)
+        b.readbits(1)
+        self.assertEqual(b.toskip(), 0b111)
+    def test_align(self) -> None:
+        b = Bitfield(io.BytesIO(bytes([2, 1, 3, 7])))
+        self.assertEqual(b.tellbits(), 0)
+        b.readbits(1)
+        self.assertEqual(b.toskip(), 0b111)
+        b.align()
+        self.assertEqual(b.tellbits(), 8)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()  # pragma: no cover
