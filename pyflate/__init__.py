28b93f84d4c5e58974ce0de9f9acfbcb6a23ac5d
Jacek Wielemborek <d33tah@gmail.com>
2024-09-19 18:41:10 +0200

    Remove one level of indentation

--- pyflate/__init__.py
+++ pyflate/__init__.py
@@ -184,146 +184,146 @@ def gzip_main(f: T.BinaryIO) -> bytes:
             # print 'linear', b.tell()[0], 'count', length, 'bits', b.tellbits() - bheader_start
             continue
 
-        if blocktype == 1 or blocktype == 2:  # Huffman
-            main_literals, main_distances = None, None
+        main_literals, main_distances = None, None
 
-            if blocktype == 1:  # Static Huffman
-                static_huffman_bootstrap = [
-                    (0, 8),
-                    (144, 9),
-                    (256, 7),
-                    (280, 8),
-                    (288, -1),
-                ]
-                static_huffman_lengths_bootstrap = [(0, 5), (32, -1)]
-                main_literals = HuffmanTable(static_huffman_bootstrap)
-                main_distances = HuffmanTable(static_huffman_lengths_bootstrap)
+        if blocktype == 1:  # Static Huffman
+            static_huffman_bootstrap = [
+                (0, 8),
+                (144, 9),
+                (256, 7),
+                (280, 8),
+                (288, -1),
+            ]
+            static_huffman_lengths_bootstrap = [(0, 5), (32, -1)]
+            main_literals = HuffmanTable(static_huffman_bootstrap)
+            main_distances = HuffmanTable(static_huffman_lengths_bootstrap)
 
-            elif blocktype == 2:  # Dynamic Huffman
-                dyna_start = b.tellbits()
-                len_codes = b.readbits(5)
-                literals = len_codes + 257
-                distances = b.readbits(5) + 1
-                code_lengths_length = b.readbits(4) + 4
-                log(
-                    "Dynamic Huffman tree: length codes: %s, distances codes: %s, code_lengths_length: %s"
-                    % (len_codes, distances, code_lengths_length)
+        elif blocktype == 2:  # Dynamic Huffman
+            dyna_start = b.tellbits()
+            len_codes = b.readbits(5)
+            literals = len_codes + 257
+            distances = b.readbits(5) + 1
+            code_lengths_length = b.readbits(4) + 4
+            log(
+                "Dynamic Huffman tree: length codes: %s, distances codes: %s, code_lengths_length: %s"
+                % (len_codes, distances, code_lengths_length)
+            )
+
+            l = [0] * 19
+            for i in range(code_lengths_length):
+                l[code_length_orders(i)] = b.readbits(3)
+            log("lengths:", l)
+
+            dynamic_codes = OrderedHuffmanTable(l)
+            dynamic_codes.populate_huffman_symbols()
+            dynamic_codes.min_max_bits()
+
+            # Decode the code_lengths for both tables at once,
+            # then split the list later
+
+            code_lengths: T.List[int] = []
+            n = 0
+            while n < (literals + distances):
+                r = dynamic_codes.find_next_symbol(b)
+                if 0 <= r <= 15:  # literal bitlength for this code
+                    count = 1
+                    what = r
+                elif r == 16:  # repeat last code
+                    count = 3 + b.readbits(2)
+                    # Is this supposed to default to '0' if in the zeroth position?
+                    what = code_lengths[-1]
+                elif r == 17:  # repeat zero
+                    count = 3 + b.readbits(3)
+                    what = 0
+                elif r == 18:  # repeat zero lots
+                    count = 11 + b.readbits(7)
+                    what = 0
+                else:
+                    raise Exception(
+                        "next code length is outside of the range 0 <= r <= 18"
+                    )
+                code_lengths += [what] * count
+                n += count
+
+            log("Literals/len lengths:", code_lengths[:literals])
+            log("Dist lengths:", code_lengths[literals:])
+            main_literals = OrderedHuffmanTable(code_lengths[:literals])
+            main_distances = OrderedHuffmanTable(code_lengths[literals:])
+            log("Read dynamic huffman tables", b.tellbits() - dyna_start, "bits")
+        else:
+            raise Exception("illegal unused blocktype in use @" + repr(b.tell()))
+
+        # Common path for both Static and Dynamic Huffman decode now
+
+        data_start = b.tell()
+        log("raw data at", data_start, "bits", b.tellbits() - bheader_start)
+        # print 'header 0 count 0 bits', b.tellbits() - bheader_start
+
+        main_literals.populate_huffman_symbols()
+        main_distances.populate_huffman_symbols()
+
+        main_literals.min_max_bits()
+        main_distances.min_max_bits()
+
+        literal_count = 0
+        literal_start = 0
+
+        while True:
+            lz_start = b.tellbits()
+            r = main_literals.find_next_symbol(b)
+            if 0 <= r <= 255:
+                if literal_count == 0:
+                    literal_start = lz_start
+                literal_count += 1
+                log("found literal", repr((r)))
+                out += bytes([r])
+            elif r == 256:
+                if literal_count > 0:
+                    # print 'add 0 count', literal_count, 'bits', lz_start-literal_start, 'data', `out[-literal_count:]`
+                    literal_count = 0
+                log("eos 0 count 0 bits", b.tellbits() - lz_start)
+                log("end of Huffman block encountered")
+                break
+            elif 257 <= r <= 285:  # dictionary lookup
+                if literal_count > 0:
+                    # print 'add 0 count', literal_count, 'bits', lz_start-literal_start, 'data', `out[-literal_count:]`
+                    literal_count = 0
+                log("reading", extra_length_bits(r), "extra bits for len")
+                length_extra = b.readbits(extra_length_bits(r))
+                length = length_base(r) + length_extra
+
+                r1 = main_distances.find_next_symbol(b)
+                if 0 <= r1 <= 29:
+                    log("reading", extra_distance_bits(r1), "extra bits for dist")
+                    distance = distance_base(r1) + b.readbits(
+                        extra_distance_bits(r1)
+                    )
+                    cached_length = length
+                    while length > distance:
+                        out += out[-distance:]
+                        length -= distance
+                    if length == distance:
+                        out += out[-distance:]
+                    else:
+                        out += out[-distance : length - distance]
+                    log("dictionary lookup: length", cached_length)
+                    log(
+                        "copy",
+                        -distance,
+                        "num bits",
+                        b.tellbits() - lz_start,
+                        "data",
+                        repr(out[-cached_length:]),
+                    )
+                elif 30 <= r1 <= 31:
+                    raise Exception(
+                        "illegal unused distance symbol in use @" + repr(b.tell())
+                    )
+            elif 286 <= r <= 287:
+                raise Exception(
+                    "illegal unused literal/length symbol in use @" + repr(b.tell())
                 )
 
-                l = [0] * 19
-                for i in range(code_lengths_length):
-                    l[code_length_orders(i)] = b.readbits(3)
-                log("lengths:", l)
-
-                dynamic_codes = OrderedHuffmanTable(l)
-                dynamic_codes.populate_huffman_symbols()
-                dynamic_codes.min_max_bits()
-
-                # Decode the code_lengths for both tables at once,
-                # then split the list later
-
-                code_lengths: T.List[int] = []
-                n = 0
-                while n < (literals + distances):
-                    r = dynamic_codes.find_next_symbol(b)
-                    if 0 <= r <= 15:  # literal bitlength for this code
-                        count = 1
-                        what = r
-                    elif r == 16:  # repeat last code
-                        count = 3 + b.readbits(2)
-                        # Is this supposed to default to '0' if in the zeroth position?
-                        what = code_lengths[-1]
-                    elif r == 17:  # repeat zero
-                        count = 3 + b.readbits(3)
-                        what = 0
-                    elif r == 18:  # repeat zero lots
-                        count = 11 + b.readbits(7)
-                        what = 0
-                    else:
-                        raise Exception(
-                            "next code length is outside of the range 0 <= r <= 18"
-                        )
-                    code_lengths += [what] * count
-                    n += count
-
-                log("Literals/len lengths:", code_lengths[:literals])
-                log("Dist lengths:", code_lengths[literals:])
-                main_literals = OrderedHuffmanTable(code_lengths[:literals])
-                main_distances = OrderedHuffmanTable(code_lengths[literals:])
-                log("Read dynamic huffman tables", b.tellbits() - dyna_start, "bits")
-            else:
-                raise Exception("illegal unused blocktype in use @" + repr(b.tell()))
-
-            # Common path for both Static and Dynamic Huffman decode now
-
-            data_start = b.tell()
-            log("raw data at", data_start, "bits", b.tellbits() - bheader_start)
-            # print 'header 0 count 0 bits', b.tellbits() - bheader_start
-
-            main_literals.populate_huffman_symbols()
-            main_distances.populate_huffman_symbols()
-
-            main_literals.min_max_bits()
-            main_distances.min_max_bits()
-
-            literal_count = 0
-            literal_start = 0
-
-            while True:
-                lz_start = b.tellbits()
-                r = main_literals.find_next_symbol(b)
-                if 0 <= r <= 255:
-                    if literal_count == 0:
-                        literal_start = lz_start
-                    literal_count += 1
-                    log("found literal", repr((r)))
-                    out += bytes([r])
-                elif r == 256:
-                    if literal_count > 0:
-                        # print 'add 0 count', literal_count, 'bits', lz_start-literal_start, 'data', `out[-literal_count:]`
-                        literal_count = 0
-                    log("eos 0 count 0 bits", b.tellbits() - lz_start)
-                    log("end of Huffman block encountered")
-                    break
-                elif 257 <= r <= 285:  # dictionary lookup
-                    if literal_count > 0:
-                        # print 'add 0 count', literal_count, 'bits', lz_start-literal_start, 'data', `out[-literal_count:]`
-                        literal_count = 0
-                    log("reading", extra_length_bits(r), "extra bits for len")
-                    length_extra = b.readbits(extra_length_bits(r))
-                    length = length_base(r) + length_extra
-
-                    r1 = main_distances.find_next_symbol(b)
-                    if 0 <= r1 <= 29:
-                        log("reading", extra_distance_bits(r1), "extra bits for dist")
-                        distance = distance_base(r1) + b.readbits(
-                            extra_distance_bits(r1)
-                        )
-                        cached_length = length
-                        while length > distance:
-                            out += out[-distance:]
-                            length -= distance
-                        if length == distance:
-                            out += out[-distance:]
-                        else:
-                            out += out[-distance : length - distance]
-                        log("dictionary lookup: length", cached_length)
-                        log(
-                            "copy",
-                            -distance,
-                            "num bits",
-                            b.tellbits() - lz_start,
-                            "data",
-                            repr(out[-cached_length:]),
-                        )
-                    elif 30 <= r1 <= 31:
-                        raise Exception(
-                            "illegal unused distance symbol in use @" + repr(b.tell())
-                        )
-                elif 286 <= r <= 287:
-                    raise Exception(
-                        "illegal unused literal/length symbol in use @" + repr(b.tell())
-                    )
         if lastbit:
             log("this was the last block, time to leave", b.tell())
             break
